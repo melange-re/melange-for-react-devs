bd09d27b7f159197740485600d186e02df83fd01
Feihong Hsu <feihong.hsu@gmail.com>
2024-01-18 09:07:30 -0600

    Add comment to explain why nodeResolve is necessary

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default defineConfig({
+  // nodeResolve is needed to import melange runtime libraries in output
+  // directory's `node_modules` directory. See
+  // https://github.com/melange-re/melange-for-react-devs/pull/16#discussion_r1455989106
   plugins: [nodeResolve()],
   server: {
     watch: {
