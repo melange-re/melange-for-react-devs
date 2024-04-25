b8bc6e6c15ff7b0df698a9b056685d30b2af57b2
Feihong Hsu <feihong.hsu@gmail.com>
2024-04-24 20:07:07 -0500

    Add Menu component

--- src/coupon-codes/Menu.jsx
+++ src/coupon-codes/Menu.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+// Fill this out after the ReasonReact version is done
+
+import React, { useState } from 'react';
+// import css from "./menu.module.css";
 
 export default function Menu() {
+  const [state, setState] = useState({
+    hotdog: {},
+    sandwich: {},
+    burger: {},
+  })
+
   return (<div>
     <h1>Menu</h1>
+
+    <table>
+      <tbody>
+      </tbody>
+    </table>
 
     <button> Checkout </button>
   </div>);
